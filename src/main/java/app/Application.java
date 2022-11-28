24ab33cea38d7f471038e4cf7464a75d396175a6
nagornovaka.24 <nagornovaksu2006@mail.ru>
2022-11-28 12:34:59 +0300

    window works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,14 +1,23 @@
 package app;
 
-import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
 public class Application implements Consumer<Event> {
-
-
+    private final Window window;
+    public Application() {
+        // создаём окно
+        window = App.makeWindow();
+        window.setEventListener(this);
+        window.setVisible(true);
+    }
     @Override
-    public void accept(Event event) {
-
+    public void accept(Event e) {
+        if (e instanceof EventWindowClose) {
+            App.terminate();
+        }else if (e instanceof EventWindowCloseRequest) {
+            window.close();
+        }
     }
 }
