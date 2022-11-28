d548010e73acde5e368436d425523c46033a288c
nagornovaka.24 <nagornovaksu2006@mail.ru>
2022-11-28 12:52:13 +0300

    java doc

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,29 +4,40 @@ import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
+/**
+ * Класс окна приложения
+ */
 public class Application implements Consumer<Event> {
     /**
-     * окно приложение
+     * окно приложения
      */
     private final Window window;
+
+    /**
+     * Конструктор окна приложения
+     */
     public Application() {
         // создаём окно
         window = App.makeWindow();
+        // задаём обработчиком событий текущий объект
         window.setEventListener(this);
+        // делаем окно видимым
         window.setVisible(true);
     }
 
     /**
      * Обработчик событий
-     * @param e the input argument
+     *
+     * @param e событие
      */
     @Override
     public void accept(Event e) {
+        // если событие - это закрытие окна
         if (e instanceof EventWindowClose) {
+            // завершаем работу приложения
             App.terminate();
-        }else if (e instanceof EventWindowCloseRequest) {
+        } else if (e instanceof EventWindowCloseRequest) {
             window.close();
-
         }
     }
 }
