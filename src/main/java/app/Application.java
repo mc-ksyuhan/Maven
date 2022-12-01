18b8917ed0f076bb9546f6b187e32edb4d7306a2
nagornovaka.24 <nagornovaksu2006@mail.ru>
2022-12-01 12:52:28 +0300

    canvas works

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,10 @@
 package app;
 
 import io.github.humbleui.jwm.*;
+import io.github.humbleui.jwm.skija.EventFrameSkija;
+import io.github.humbleui.skija.Surface;
 
+import java.io.File;
 import java.util.function.Consumer;
 
 /**
@@ -32,6 +35,26 @@ public class Application implements Consumer<Event> {
             case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
             case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
         }
+        // названия слоёв, которые будем перебирать
+        String[] layerNames = new String[]{
+                "LayerGLSkija", "LayerRasterSkija"
+        };
+
+        // перебираем слои
+        for (String layerName : layerNames) {
+            String className = "io.github.humbleui.jwm.skija." + layerName;
+            try {
+                Layer layer = (Layer) Class.forName(className).getDeclaredConstructor().newInstance();
+                window.setLayer(layer);
+                break;
+            } catch (Exception e) {
+                System.out.println("Ошибка создания слоя " + className);
+            }
+        }
+
+        // если окну не присвоен ни один из слоёв
+        if (window._layer == null)
+            throw new RuntimeException("Нет доступных слоёв для создания");
         window.setVisible(true);
     }
 
@@ -48,6 +71,12 @@ public class Application implements Consumer<Event> {
             App.terminate();
         } else if (e instanceof EventWindowCloseRequest) {
             window.close();
+        }
+        else if (e instanceof EventFrameSkija ee) {
+            // получаем поверхность рисования
+            Surface s = ee.getSurface();
+            // очищаем её канвас заданным цветом
+            s.getCanvas().clear(0xFF264653);
         }
     }
 }
