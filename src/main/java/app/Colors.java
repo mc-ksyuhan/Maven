d3229cbbfb63073dbf31d5f779344af21bf18e2b
nagornovaka.24 <nagornovaksu2006@mail.ru>
2023-01-16 12:20:23 +0300

    inputs work

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -21,6 +21,14 @@ public static final int LABEL_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
     /**
      * Цвет текста
      */
+    /**
+     * Цвет подложки поля ввода
+     */
+    public static final int FIELD_BACKGROUND_COLOR = Misc.getColor(255, 255, 255, 255);
+    /**
+     * Цвет текста
+     */
+    public static final int FIELD_TEXT_COLOR = Misc.getColor(255, 0, 0, 0);
     public static final int MULTILINE_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
     /**
      * Запрещённый конструктор
--- src/main/java/panels/PanelRendering.java
+++ src/main/java/panels/PanelRendering.java
@@ -24,6 +24,20 @@ public class PanelRendering extends GridPanel {
     public static Task task;
 
     /**
+     * Сохранить файл
+     */
+    public static void save() {
+        PanelLog.info("save");
+    }
+
+    /**
+     * Загрузить файл
+     */
+    public static void load() {
+        PanelLog.info("load");
+    }
+
+    /**
      * Панель управления
      *
      * @param window     окно
@@ -79,18 +93,15 @@ public class PanelRendering extends GridPanel {
      */
     @Override
     public void accept(Event e) {
-        // вызов обработчика предка
+        // вызываем обработчик предка
         super.accept(e);
-        // если событие - это клик мышью
         if (e instanceof EventMouseButton ee) {
             // если последнее положение мыши сохранено и курсор был внутри
-            if (lastMove != null && lastInside){
-            // если событие - нажатие мыши
+            if (lastMove != null && lastInside) {
+                // если событие - нажатие мыши
                 if (ee.isPressed())
-                // обрабатываем клик по задаче
-                task.click(lastWindowCS.getRelativePos(lastMove), ee.getButton());
-                // перерисовываем окно
-                window.requestFrame();
+                    // обрабатываем клик по задаче
+                    task.click(lastWindowCS.getRelativePos(lastMove), ee.getButton());
             }
         }
     }
