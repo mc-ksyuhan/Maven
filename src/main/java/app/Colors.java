7e8de1c976804c025fd7379a8bfaa8c8badc9f34
nagornovaka.24 <nagornovaksu2006@mail.ru>
2023-01-16 11:41:46 +0300

    multiline label

--- src/main/java/app/Colors.java
+++ src/main/java/app/Colors.java
@@ -18,7 +18,10 @@ public static final int LABEL_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
      * цвет фона
      */
     public static final int APP_BACKGROUND_COLOR = Misc.getColor(255, 38, 70, 83);
-
+    /**
+     * Цвет текста
+     */
+    public static final int MULTILINE_TEXT_COLOR = Misc.getColor(64, 255, 255, 255);
     /**
      * Запрещённый конструктор
      */
