05d4265b4c8c7b1ee13ffdc0e75c80742b2f77a6
nagornovaka.24 <nagornovaksu2006@mail.ru>
2023-02-16 11:37:56 +0300

    scale works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -75,6 +75,10 @@ public class Task {
      * будет нарисована увеличенная
      */
     private static final int DELIMITER_ORDER = 10;
+    /**
+     * коэффициент колёсика мыши
+     */
+    private static final float WHEEL_SENSITIVE = 0.001f;
 
     /**
      * Задача
@@ -284,5 +288,19 @@ public class Task {
             }
         }
         canvas.restore();
+    }
+
+    /**
+     * Масштабирование области просмотра задачи
+     *
+     * @param delta  прокрутка колеса
+     * @param center центр масштабирования
+     */
+    public void scale(float delta, Vector2i center) {
+        if (lastWindowCS == null) return;
+        // получаем координаты центра масштабирования в СК задачи
+        Vector2d realCenter = ownCS.getCoords(center, lastWindowCS);
+        // выполняем масштабирование
+        ownCS.scale(1 + delta * WHEEL_SENSITIVE, realCenter);
     }
 }
